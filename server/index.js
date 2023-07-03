a78745ad515a364c3d6188ca7dbd285375cfe74a
miinje <njjj00083@gmail.com>
2023-07-03 14:21:46 +0900

    자바스크립트 eslint, prettier 학습

---
+++ server/index.js
@@ -0,0 +1,11 @@
+// node server/index.js
+const liveServer = require('live-server');
+
+const params = {
+  port: 5500,
+  host: 'localhost',
+  root: './client',
+  open: false
+}
+
+liveServer.start(params);
